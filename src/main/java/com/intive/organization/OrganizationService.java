db2f113115d4f89cc06e7971b28360c3c1440754
Maciej Wójcik <wojcik91@gmail.com>
2019-01-03 22:30:00 +0100

    Implement organization service

---
+++ src/main/java/com/intive/organization/OrganizationService.java
@@ -0,0 +1,38 @@
+package com.intive.organization;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class OrganizationService {
+
+    @Autowired
+    private OrganizationRepository organizationRepository;
+
+    public List<Organization> getAllOrganizations() {
+
+        List<Organization> organizations = new ArrayList<>();
+        organizationRepository.findAll().forEach(organizations::add);
+        return organizations;
+    }
+
+    public Optional<Organization> getOrganization(String name) {
+        return organizationRepository.findById(name);
+    }
+
+    public void addOrganization(Organization organization) {
+        organizationRepository.save(organization);
+    }
+
+    public void updateOrganization(Organization organization) {
+        organizationRepository.save(organization);
+    }
+
+    public void deleteOrganization(String name) {
+        organizationRepository.deleteById(name);
+    }
+}
