e345ff1ae7b02bfd688d7d8419cc72f69613d783
Maciej Wójcik <wojcik91@gmail.com>
2019-01-04 22:34:19 +0100

    Implement reservation JPA service

--- src/main/java/com/intive/reservation/ReservationService.java
+++ src/main/java/com/intive/reservation/ReservationService.java
@@ -1,4 +1,37 @@
 package com.intive.reservation;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class ReservationService {
+
+    @Autowired
+    private ReservationRepository reservationRepository;
+
+    public List<Reservation> getAllReservations(Long roomId) {
+        List<Reservation> reservations = new ArrayList<>();
+        reservationRepository.findByRoomId(roomId).forEach(reservations::add);
+        return reservations;
+    }
+
+    public Optional<Reservation> getReservation(Long id) {
+        return reservationRepository.findById(id);
+    }
+
+    public void addReservation(Reservation reservation) {
+        reservationRepository.save(reservation);
+    }
+
+    public void updateReservation(Reservation reservation) {
+        reservationRepository.save(reservation);
+    }
+
+    public void deleteReservation(Long id) {
+        reservationRepository.deleteById(id);
+    }
 }
