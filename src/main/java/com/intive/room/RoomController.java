64693e29165a37ead76b1269f85d69335417c6c6
Maciej Wójcik <wojcik91@gmail.com>
2019-01-04 09:25:19 +0100

    Add method stubs

--- src/main/java/com/intive/room/RoomController.java
+++ src/main/java/com/intive/room/RoomController.java
@@ -1,4 +1,33 @@
 package com.intive.room;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
 public class RoomController {
+
+    @Autowired
+    private RoomService roomService;
+
+    public List<Room> getAllRooms() {
+
+    }
+
+    public Room getRoom() {
+
+    }
+
+    public void addRoom() {
+
+    }
+
+    public void updateRoom() {
+
+    }
+
+    public void deleteRoom() {
+
+    }
 }
