3d2aa748cb596dff9149aba1bfc39da3942e147f
Maciej Wójcik <wojcik91@gmail.com>
2019-01-04 09:34:26 +0100

    Map methods to API endpoints

--- src/main/java/com/intive/room/RoomController.java
+++ src/main/java/com/intive/room/RoomController.java
@@ -1,6 +1,8 @@
 package com.intive.room;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -11,22 +13,27 @@ public class RoomController {
     @Autowired
     private RoomService roomService;
 
+    @RequestMapping("/organizations/{id}/rooms")
     public List<Room> getAllRooms() {
 
     }
 
+    @RequestMapping("/organizations/{id}/rooms/{roomId}")
     public Room getRoom() {
 
     }
 
+    @RequestMapping(value = "/organizations/{id}/rooms", method = RequestMethod.POST)
     public void addRoom() {
 
     }
 
+    @RequestMapping(value = "/organizations/{id}/rooms/{roomId}", method = RequestMethod.PUT)
     public void updateRoom() {
 
     }
 
+    @RequestMapping(value = "/organizations/{id}/rooms/{roomId}", method = RequestMethod.DELETE)
     public void deleteRoom() {
 
     }
